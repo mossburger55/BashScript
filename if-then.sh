c59c193557b60f1f163a98fbd8ce15c8ae54f7ec
mossburger55 <84609066+mossburger55@users.noreply.github.com>
2024-08-23 11:15:47 -0600

    Update if-then.sh
    Adding #comparison and #file operation sections

--- if-then.sh
+++ if-then.sh
@@ -66,3 +66,23 @@ then
 echo You should try IT, it’s a good field
 echo
 fi
+
+
+
+# Comparisons:
+-eq equal to for numbers
+== equal to for letters
+-ne not equal to
+!== not equal to for letters
+-lt less than
+-le less than or equal to
+-gt greater than
+-ge greater than or equal to
+
+# File Operations:
+-s file exists and is not empty
+-f file exists and is not a directory
+-d directory exists
+-x file is executable
+-w file is writable
+-r file is readabl
