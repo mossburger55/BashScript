c9af614dd2d02568132ae7ad4efdb855b362cdc5
mossburger55 <84609066+mossburger55@users.noreply.github.com>
2024-08-27 16:09:45 -0600

    Create change-permission.sh
    this script will create a file and change permissions

---
+++ change-permission.sh
@@ -0,0 +1,9 @@
+# Author: mossburger55
+# Date Aug 27, 2024
+# Description: this script will create a file and change permissions 
+
+#!/bin/bash 
+touch new-file
+chmod a+r new-file
+chmod a+w new-file
+chmod a+x new-file
